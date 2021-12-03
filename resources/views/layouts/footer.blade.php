81a903ef40daac99dec2d73b7f8e387e5af413db
GhaythBg <ghayth.bengara@sesame.com.tn>
2021-12-03 12:28:04 +0100

    Créer les layouts scripts et footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,78 @@
+<div class = "container">
+    <div class = "row">
+        <div class = "col-lg-2 col-md-6 col-sm-6">
+            <div class = "single-footer-widget">
+                <h4>Top Products</h4>
+                <ul>
+                    <li><a href = "#">Managed Website</a></li>
+                    <li><a href = "#">Manage Reputation</a></li>
+                    <li><a href = "#">Power Tools</a></li>
+                    <li><a href = "#">Marketing Service</a></li>
+                </ul>								
+            </div>
+        </div>
+        <div class = "col-lg-2 col-md-6 col-sm-6">
+            <div class = "single-footer-widget">
+                <h4>Liens rapides</h4>
+                <ul>
+                    <li><a href = "#">Jobs</a></li>
+                    <li><a href = "#">Brand Assets</a></li>
+                    <li><a href = "#">Investor Relations</a></li>
+                    <li><a href = "#">Terms of Service</a></li>
+                </ul>								
+            </div>
+        </div>
+        <div class = "col-lg-2 col-md-6 col-sm-6">
+            <div class = "single-footer-widget">
+                <h4>Features</h4>
+                <ul>
+                    <li><a href = "#">Jobs</a></li>
+                    <li><a href = "#">Brand Assets</a></li>
+                    <li><a href = "#">Investor Relations</a></li>
+                    <li><a href = "#">Conditions d'utilisation</a></li>
+                </ul>								
+            </div>
+        </div>
+        <div class = "col-lg-2 col-md-6 col-sm-6">
+            <div class = "single-footer-widget">
+                <h4>Resources</h4>
+                <ul>
+                    <li><a href = "#">Guides</a></li>
+                    <li><a href = "#">Research</a></li>
+                    <li><a href = "#">Experts</a></li>
+                    <li><a href = "#">Agencies</a></li>
+                </ul>								
+            </div>
+        </div>																		
+        <div class = "col-lg-4  col-md-6 col-sm-6">
+            <div class = "single-footer-widget">
+                <h4>Abonnement</h4>
+                <p>Restez à jour avec nos dernières</p>
+                <div class = "" id = "mc_embed_signup">
+                    <form name = "form_abonnement" id = "form_abonnement" target = "_blank" action = "#" method = "post">
+                        <div class = "input-group">
+                            <input type = "email" class = "form-control" name = "email_abonnement" id = "email_abonnement" placeholder = "Saisissez votre adresse email.." onfocus = "this.placeholder = ''" onblur = "this.placeholder = 'Saisissez votre adresse email.. '" required>
+                            <div class = "input-group-btn">
+                                <button class = "btn btn-default" type="submit">
+                                    <span class = "lnr lnr-arrow-right"></span>
+                                </button>    
+                            </div>
+                            <div class = "info"></div>  
+                        </div>
+                    </form> 
+                </div>
+            </div>
+        </div>											
+    </div>
+    <div class = "footer-bottom row align-items-center justify-content-between">
+        <p class = "footer-text m-0 col-lg-6 col-md-12">
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous les droits sont réservés | Développé par <i class = "fa fa-heart-o" aria-hidden = "true"></i> <a href = "#" target = "_blank">STAGI </a><i class = "fa fa-heart-o" aria-hidden = "true"></i>
+        </p>
+        <div class = "col-lg-6 col-sm-12 footer-social">
+            <a href = "#"><i class = "fa fa-facebook"></i></a>
+            <a href = "#"><i class = "fa fa-twitter"></i></a>
+            <a href = "#"><i class = "fa fa-instagram"></i></a>
+            <a href = "#"><i class = "fa fa-behance"></i></a>
+        </div>
+    </div>						
+</div>
---
+++ resources/views/layouts/menu-site.blade.php
@@ -0,0 +1,14 @@
+<div class = "row align-items-center justify-content-between d-flex">
+    <div id = "logo">
+        <a href = "{{url('/')}}">
+            <img src = "{{asset('img/logos/favicon.png')}}" alt = "" title = ""/>
+        </a>
+        <h1>Stagi</h1>
+    </div>
+    <nav id = "nav-menu-container">
+        <ul class = "nav-menu">
+            <li><a href = "{{url('/')}}">Accueil</a></li>	          					          		          
+            <li><a href = "contact.html">Contact</a></li>
+        </ul>
+    </nav>		    		
+</div>
