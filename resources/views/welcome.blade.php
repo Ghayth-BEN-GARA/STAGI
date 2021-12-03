46c55cc2e70171199d4a9496a65696bf282649ec
GhaythBg <ghayth.bengara@sesame.com.tn>
2021-12-03 11:39:53 +0100

    Créer le layout head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,7 @@
 <!DOCTYPE html>
     <html lang = "zxx" class="no-js">
 	    <head>
-		    <meta name = "viewport" content = "width=device-width, initial-scale=1, shrink-to-fit=no">
-		    <link rel = "shortcut icon" href = "{{asset('img/logos/favicon.png')}}">
-		    <meta name = "author" content = "STAGI">
-		    <meta name = "description" content = "">
-		    <meta name = "keywords" content = "">
-		    <meta charset = "UTF-8">
-		    <title>Accueil | STAGI</title>
-		    <link href = "https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel = "stylesheet"> 
-			<link rel = "stylesheet" href = "{{asset('css/linearicons.css')}}">
-			<link rel = "stylesheet" href = "{{asset('css/font-awesome.min.css')}}">
-			<link rel = "stylesheet" href = "{{asset('css/bootstrap.css')}}">
-			<link rel = "stylesheet" href = "{{asset('css/magnific-popup.css')}}">
-			<link rel = "stylesheet" href = "{{asset('css/nice-select.css')}}">							
-			<link rel = "stylesheet" href = "{{asset('css/animate.min.css')}}">
-			<link rel = "stylesheet" href = "{{asset('css/owl.carousel.css')}}">			
-			<link rel = "stylesheet" href = "{{asset('css/jquery-ui.css')}}">			
-			<link rel = "stylesheet" href = "{{asset('css/main.css')}}">
+		    @include('layouts.head',['nom_page'=>'Accueil'])
 		</head>
 		<body>	
 		    <header id="header" id="home">
